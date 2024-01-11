5c89eb813bc6365a5f94ad01dbf4265a0c38b617
Vozzhd <Vozzhd@yandex.ru>
2024-01-11 19:39:21 +0300

    Sprint №9 HomeWork №2 is done. Added: -listeners to buttons in SettingsActivity, all buttons in this activity works -string-resourses - Changed: -corrected theme in AndroidManifest.xml

--- app/src/main/java/com/practicum/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker/SettingsActivity.kt
@@ -1,9 +1,12 @@
 package com.practicum.playlistmaker
+
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.widget.ImageButton
 import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
+
 
 class SettingsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,17 +14,43 @@ class SettingsActivity : AppCompatActivity() {
         setContentView(R.layout.activity_settings)
 
         val backToMainButton = findViewById<ImageButton>(R.id.backButton)
-        backToMainButton.setOnClickListener{
+        backToMainButton.setOnClickListener {
             finish()
         }
 
         val shareButton = findViewById<ImageView>(R.id.shareButton)
-        shareButton.setOnClickListener{
+        shareButton.setOnClickListener {
             val intent = Intent()
-            intent.action=Intent.ACTION_SEND
-            intent.putExtra(Intent.EXTRA_TEXT, "https://practicum.yandex.ru/");
-            intent.type="application/octet-stream"
-            startActivity(Intent.createChooser(intent,"share to:"))
+            val sharedLink = getString(R.string.linkInsideShareButton)
+            intent.action = Intent.ACTION_SEND
+            intent.putExtra(Intent.EXTRA_TEXT, sharedLink);
+            intent.type = "application/octet-stream"
+            startActivity(Intent.createChooser(intent, "share to:"))
         }
+
+        val supportButton = findViewById<ImageView>(R.id.supportButton)
+        supportButton.setOnClickListener {
+
+            val intent = Intent(Intent.ACTION_SENDTO)
+            intent.data = Uri.parse("mailto:")
+
+            val subject = getString(R.string.supportLetterSubject)
+            val message = getString(R.string.supportLetterText)
+            val addressee = getString(R.string.supportLetterAddressee)
+
+            intent.putExtra(Intent.EXTRA_EMAIL, arrayOf(addressee))
+            intent.putExtra(Intent.EXTRA_SUBJECT, subject)
+            intent.putExtra(Intent.EXTRA_TEXT, message)
+
+            startActivity(intent)
+        }
+
+        val userAgreementButton = findViewById<ImageView>(R.id.userAgreementButton)
+        userAgreementButton.setOnClickListener {
+            val linkToTermsOfUse = getString(R.string.linkToTermsOfUse)
+            val intent = Intent(Intent.ACTION_VIEW, Uri.parse(linkToTermsOfUse))
+            startActivity(intent)
+        }
+
     }
 }
