4ae9a74b6d670b1d60c3a7aee27253b5d8872fcc
Vozzhd <Vozzhd@yandex.ru>
2024-01-13 22:33:18 +0300

    Sprint №9 HomeWork №3 is done. Added: -icons -custom background of TextView -string resourses Changed: -add work with edit text in settingsActivity -add new colors in themes Tertiary(day-gray/night-black)

--- app/src/main/java/com/practicum/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker/SearchActivity.kt
@@ -2,10 +2,46 @@ package com.practicum.playlistmaker
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.View
+import android.widget.EditText
+import android.widget.ImageView
 
 class SearchActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search)
+
+        val inputEditText = findViewById<EditText>(R.id.findField)
+        val clearButton = findViewById<ImageView>(R.id.clearButton)
+
+        clearButton.setOnClickListener {
+            inputEditText.setText("")
+        }
+
+        fun clearButtonVisibility(s: CharSequence?): Int {
+            if (s.isNullOrEmpty()) {
+                return View.GONE
+            } else {
+                return View.VISIBLE
+            }
+        }
+
+        val simpleTextWatcher = object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+                // empty
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                clearButton.visibility = clearButtonVisibility(p0)
+            }
+
+            override fun afterTextChanged(p0: Editable?) {
+                // empty
+            }
+
+        }
+        inputEditText.addTextChangedListener(simpleTextWatcher)
     }
 }
