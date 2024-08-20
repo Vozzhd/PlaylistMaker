fe5c2d35b4bffed6b0c703ff1f96c4e0d3ec4268
Vozzhd <Vozzhd@yandex.ru>
2024-08-20 23:03:51 +0300

    Sprint 22.6 Промежуточный коммит, исправлен фрейм создания плейлиста, стоки и размеры вынесены в ресурсы

--- app/src/main/java/com/practicum/playlistmaker/playlistCreating/ui/PlaylistManagerFragment.kt
+++ app/src/main/java/com/practicum/playlistmaker/playlistCreating/ui/PlaylistManagerFragment.kt
@@ -1,18 +1,23 @@
 package com.practicum.playlistmaker.playlistCreating.ui
 
+import android.content.res.ColorStateList
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.activity.OnBackPressedCallback
-import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.PickVisualMediaRequest
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.ContextCompat
 import androidx.core.widget.addTextChangedListener
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import com.google.android.material.textfield.TextInputLayout
 import com.practicum.playlistmaker.R
 import com.practicum.playlistmaker.databinding.FragmentPlaylistManagerBinding
 import com.practicum.playlistmaker.playlistCreating.ui.model.PlaylistManagerViewModel
@@ -23,7 +28,6 @@ class PlaylistManagerFragment : Fragment() {
 
     private var _binding: FragmentPlaylistManagerBinding? = null
     private val binding get() = _binding!!
-
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -36,13 +40,12 @@ class PlaylistManagerFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         val viewModel: PlaylistManagerViewModel by viewModel()
-        val pickMedia: ActivityResultLauncher<PickVisualMediaRequest>
 
         val albumNameTextWatcher = object : TextWatcher {
             override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
             override fun onTextChanged(text: CharSequence?, start: Int, before: Int, count: Int) {
                 createButtonState(text)
-
+                frameAndTextColorsState(binding.albumNameFrame, text)
                 viewModel.setPlaylistName(text)
             }
 
@@ -53,15 +56,24 @@ class PlaylistManagerFragment : Fragment() {
             override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
             override fun onTextChanged(text: CharSequence?, start: Int, before: Int, count: Int) {
                 viewModel.setPlaylistDescription(text)
+                frameAndTextColorsState(binding.descriptionNameFrame, text)
             }
 
             override fun afterTextChanged(s: Editable?) {}
         }
 
-
+        val pickMedia =
+            registerForActivityResult(ActivityResultContracts.PickVisualMedia()) { uri ->
+                if (uri != null) {
+                    binding.shapableImageView.setImageURI(uri)
+                    viewModel.setUri(uri)
+                }
+            }
+        binding.shapableImageView.setOnClickListener {
+            pickMedia.launch(PickVisualMediaRequest(ActivityResultContracts.PickVisualMedia.ImageOnly))
+        }
         binding.name.addTextChangedListener(albumNameTextWatcher)
-        binding.description.addTextChangedListener { albumDescriptionTextWatcher }
-
+        binding.description.addTextChangedListener(albumDescriptionTextWatcher)
 
         binding.backButton.setOnClickListener {
             if (!viewModel.emptyCheck()) {
@@ -84,6 +96,15 @@ class PlaylistManagerFragment : Fragment() {
 
         binding.createButton.setOnClickListener {
             viewModel.createPlaylist()
+            Toast.makeText(
+                requireContext(),
+                "${requireContext().getString(R.string.playlist)} ${binding.name.text} ${
+                    requireContext().getString(R.string.created)
+                }.",
+                Toast.LENGTH_SHORT
+            ).show()
+
+            findNavController().popBackStack()
         }
 
 
@@ -102,5 +123,78 @@ class PlaylistManagerFragment : Fragment() {
                 findNavController().popBackStack()
             }
             .show()
+    }
+
+    private fun frameAndTextColorsState(frame: TextInputLayout, text: CharSequence?) {
+        if (!text.isNullOrEmpty()) {
+            frame.defaultHintTextColor =
+                ColorStateList.valueOf(
+                    ContextCompat.getColor(
+                        requireContext(),
+                        R.color.YP_blue
+                    )
+                )
+
+            frame.setBoxStrokeColorStateList(
+                ColorStateList(
+                    arrayOf(
+                        intArrayOf(-android.R.attr.state_enabled),
+                        intArrayOf(android.R.attr.state_hovered, android.R.attr.state_enabled),
+                        intArrayOf(android.R.attr.state_focused, android.R.attr.state_enabled)
+                    ),
+                    intArrayOf(
+                        ContextCompat.getColor(
+                            requireContext(),
+                            R.color.YP_blue
+                        ),
+                        ContextCompat.getColor(
+                            requireContext(),
+                            R.color.YP_text_gray
+                        ),
+                        ContextCompat.getColor(
+                            requireContext(),
+                            R.color.YP_blue
+                        )
+                    )
+                )
+            )
+        } else {
+            frame.defaultHintTextColor =
+                ColorStateList.valueOf(
+                    ContextCompat.getColor(
+                        requireContext(),
+                        R.color.YP_text_gray
+                    )
+                )
+            frame.hintTextColor = ColorStateList.valueOf(
+                ContextCompat.getColor(
+                    requireContext(),
+                    R.color.YP_blue
+                )
+            )
+            frame.setBoxStrokeColorStateList(
+                ColorStateList(
+                    arrayOf(
+                        intArrayOf(-android.R.attr.state_enabled),
+                        intArrayOf(android.R.attr.state_hovered, android.R.attr.state_enabled),
+                        intArrayOf(android.R.attr.state_focused, android.R.attr.state_enabled)
+                    ),
+                    intArrayOf(
+                        ContextCompat.getColor(
+                            requireContext(),
+                            R.color.YP_text_gray
+                        ),
+                        ContextCompat.getColor(
+                            requireContext(),
+                            R.color.YP_text_gray
+                        ),
+                        ContextCompat.getColor(
+                            requireContext(),
+                            R.color.YP_blue
+                        )
+                    )
+                )
+            )
+        }
     }
 }
